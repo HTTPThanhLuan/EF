b4b464df282c75f841e042265c8a8502819eb8c4
ThanhLuan <luan.tt87@gmail.com>
2020-06-09 14:06:57 -0500

    database first

---
+++ Database-First/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Database_FirstEntities();
+            var post = new Post()
+            {
+                Title = "title",
+                Body = "Body",
+                Date = DateTime.Now
+            };
+            context.Posts.Add(post); // in memory
+            context.SaveChanges(); //commit to db
+        }
+    }
+}
---
+++ Database-First/ReadMe.txt
@@ -0,0 +1,4 @@
+﻿1. design schema.
+2. create solution Console App (.net framework )
+3. install-package entityframework using Package Manager Console
+4. Create new Item / ADO.NET Entity Data model
