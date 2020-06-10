1f376d1751f18e7953626bfef27d505daae3856a
ThanhLuan <luan.tt87@gmail.com>
2020-06-10 14:53:00 -0500

    pluto database first

---
+++ Pluto-Database-First/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pluto_Database_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var context = new plutoContext();
+            var courses = context.GetCourses();
+            foreach (var item in courses)
+            {
+                Console.WriteLine(item.Title);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Pluto-Database-First/ReadMe.txt
@@ -0,0 +1,3 @@
+﻿1. Settup Database with Script
+2. Add new Item / ADO.NET entity data model
+3. You will see model.edmx diagram
