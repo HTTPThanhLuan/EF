297750f7e2db78d5be4fc3de75ab412b390eb69b
ThanhLuan <luan.tt87@gmail.com>
2020-06-10 23:07:07 -0500

    database first workflow exercise

---
+++ VidzyMovieRentalStore/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VidzyMovieRentalStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new VidzyContext();
+            var video = new Video()
+            {
+                Name = "name",
+                ReleaseDate = DateTime.Now,
+                GenreId=2
+            };
+
+            //  context.Videos.Add(video);
+            //   context.SaveChanges();
+
+
+
+            //add video by calling a store procedure 
+            context.AddVideo("name7", DateTime.Now, "Thriller",Classification.Platium);
+
+
+
+        }
+    }
+}
---
+++ VidzyMovieRentalStore/ReadMe.txt
@@ -0,0 +1,1 @@
+﻿read exercise file
